0792dcc95c97790b9059d0794996ba3c7e8f2785
victor kigen <vickigen@gmail.com>
2021-09-06 16:13:08 +0300

    added job class

--- Ajira_Network/Jobs/models.py
+++ Ajira_Network/Jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Job(models.Model):
+    position_name = models.CharField(max_length=100)
+    text_description = models.TextField()
+    min_age = models.IntegerField()
+    max_age = models.IntegerField()
+    salary= models.IntegerField()
+    n_openings = models.IntegerField()
+    

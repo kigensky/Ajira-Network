3f4f1833f98ab6851ad85191e6cebd9369b77507
victor kigen <vickigen@gmail.com>
2021-09-07 11:49:23 +0300

    added a class

--- Ajira_Network/Jobs/admin.py
+++ Ajira_Network/Jobs/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from models import Job
+from .models import Job
 
 # Register your models here.
 
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    exclude = ("creater",)
+    list_display = ("position_name","creater",)
+    
+    def save_model(self, request, obj, form, change):
+        obj.creater = request.user
+        obj.save()
+
+admin.site.register(Job, JobAdmin)
 

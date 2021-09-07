3263f7584c48d97b7d1ee56e4b7272b5dcfea38a
victor kigen <vickigen@gmail.com>
2021-09-07 13:12:30 +0300

    added filter option

--- Ajira_Network/Jobs/admin.py
+++ Ajira_Network/Jobs/admin.py
@@ -7,6 +7,20 @@ class JobAdmin(admin.ModelAdmin):
     exclude = ("creater",)
     list_display = ("position_name","creater",)
     
+    
+    def get_queryset(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return Job.objects.all()
+        else:
+            return Job.objects.filter(creater = request.user)
+        
+    
+    def get_list_display(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return("position_name","creater",)
+        else:
+            return("position_name",)
+    
     def save_model(self, request, obj, form, change):
         obj.creater = request.user
         obj.save()
